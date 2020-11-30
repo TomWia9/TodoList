ea9de97533ef0b7c9c481a3c401074fbebbc6ec3
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2020-11-30 18:58:36 +0100

    Remove template files, add AllTodo component, add Todo controller

---
+++ TodoList/Client/Pages/AllTodoBase.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using TodoList.Shared.Dto;
+
+namespace TodoList.Client.Pages
+{
+    public partial class AllTodoBase : ComponentBase
+    {
+        [Inject]
+        protected HttpClient HttpClient { get; set; }
+        public IEnumerable<TodoDTO> Todos { get; set; } = new List<TodoDTO>();
+
+        protected override async Task OnInitializedAsync()
+        {
+            Todos = await HttpClient.GetFromJsonAsync<IEnumerable<TodoDTO>>("api/todo");
+        }
+    }
+}
---
+++ TodoList/Server/Controllers/Todo.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoList.Shared.Dto;
+
+namespace TodoList.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Todo : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TodoDTO>>> GetAllTodos()
+        {
+            IList<TodoDTO> todos = new List<TodoDTO>()
+            {
+               new TodoDTO()
+               {
+                   Id = 1,
+                   Title = "First ToDo",
+                   Description = "Example description",
+               },
+               new TodoDTO()
+               {
+                   Id = 2,
+                   Title = "Second ToDo",
+                   Description = "Example second description",
+                   IsDone = true
+               },
+               new TodoDTO()
+               {
+                   Id = 3,
+                   Title = "Third ToDo",
+                   Description = "Example third description",
+               }
+
+            };
+
+            return Ok(todos);
+        }
+    }
+}
