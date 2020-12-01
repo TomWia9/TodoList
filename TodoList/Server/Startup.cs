4f17ba332d77f61f921a5e9c34c21549eb4126b1
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2020-12-01 10:50:14 +0100

    Add swagger

--- TodoList/Server/Startup.cs
+++ TodoList/Server/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
 using System.Linq;
 using TodoList.Server.Models;
 using TodoList.Server.Repositories;
@@ -37,6 +38,29 @@ namespace TodoList.Server
 
             services.AddAutoMapper(typeof(Startup));
 
+            services.AddSwaggerGen(setupAction =>
+            {
+                setupAction.SwaggerDoc(
+                    "TodoListOpenAPISepcification",
+                    new Microsoft.OpenApi.Models.OpenApiInfo()
+                    {
+                        Title = "HotelManagement API",
+                        Version = "1",
+                        //Description = "",
+                        Contact = new Microsoft.OpenApi.Models.OpenApiContact()
+                        {
+                            Email = "tomaszwiatrowski9@gmail.com",
+                            Name = "Tomasz Wiatrowski",
+                            Url = new Uri("https://www.linkedin.com/in/tomasz-wiatrowski-279b00176/")
+                        },
+                        License = new Microsoft.OpenApi.Models.OpenApiLicense()
+                        {
+                            Name = "MIT License",
+                            Url = new Uri("https://opensource.org/licenses/MIT")
+                        }
+                    });
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -53,6 +77,18 @@ namespace TodoList.Server
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(setupAction =>
+            {
+                setupAction.SwaggerEndpoint("/swagger/TodoListOpenAPISepcification/swagger.json", "TodoList API");
+                setupAction.RoutePrefix = "swagger";
+
+                setupAction.DefaultModelExpandDepth(2);
+                setupAction.DefaultModelRendering(Swashbuckle.AspNetCore.SwaggerUI.ModelRendering.Model);
+                setupAction.DocExpansion(Swashbuckle.AspNetCore.SwaggerUI.DocExpansion.None);
+            });
 
             app.UseHttpsRedirection();
             app.UseBlazorFrameworkFiles();
