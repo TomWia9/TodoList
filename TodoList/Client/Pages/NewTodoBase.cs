59d7a6262b8790996335865dd683b8b3ac1067cc
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2020-12-09 23:39:25 +0100

    Add newTodo component

---
+++ TodoList/Client/Pages/NewTodoBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using TodoList.Shared.Dto;
+
+namespace TodoList.Client.Pages
+{
+    public class NewTodoBase : ComponentBase
+    {
+        [Inject]
+        protected HttpClient HttpClient { get; set; }
+
+        [Parameter]
+        public int ListId { get; set; }
+
+        protected TodoForCreationDto TodoForCreation { get; set; } = new TodoForCreationDto();
+
+        protected async Task CreateTodo()
+        {
+            Console.WriteLine("CreateTodo");
+            Console.WriteLine("ListId: {0}", ListId);
+            //not implemented yet
+        }
+    }
+}
