bf9e59c28f1219415d5187f6c5cb570cee7fd85d
Tomasz Wiatrowski <tomaszwiatrowski9@gmail.com>
2021-01-10 21:31:24 +0100

    Add LoginBase class

---
+++ TodoList/Client/Components/LoginBase.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using TodoList.Client.Helpers.ExtensionMethods;
+using TodoList.Client.Services;
+using TodoList.Shared.Auth;
+
+namespace TodoList.Client.Components
+{
+    public class LoginBase : ComponentBase
+    {
+        [Inject]
+        protected NavigationManager NavigationManager { get; set; }
+        
+        [Inject]
+        protected IAuthenticationService AuthenticationService { get; set; }
+        
+        protected readonly AuthenticateRequest AuthenticateRequest = new();
+        protected bool Loading;
+        protected string Error;
+        protected async void HandleValidSubmit()
+        {
+            Loading = true;
+            try
+            {
+                await AuthenticationService.Login(AuthenticateRequest.Username, AuthenticateRequest.Password);
+                var returnUrl = NavigationManager.QueryString("returnUrl") ?? "/";
+                NavigationManager.NavigateTo(returnUrl);
+            }
+            catch (Exception ex)
+            {
+                Error = ex.Message;
+                Loading = false;
+                StateHasChanged();
+            }
+        }
+    }
+}
